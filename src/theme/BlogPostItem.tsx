04560fa9e429f71e90878b43eb346c3a7694ba1a
brown <brown2243@naver.com>
2024-03-06 23:13:26 +0900

    feat: path 분기 추가

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import OriginalBlogPostItem from "@theme-original/BlogPostItem";
+import { useLocation } from "@docusaurus/router";
 import Comment from "@site/src/components/Comment";
 
+const BASIC_PATH = "/";
 function BlogPostItem(props) {
+  const { pathname } = useLocation();
+  const isBasicPath = BASIC_PATH === pathname;
+
   return (
     <>
       <OriginalBlogPostItem {...props} />
-      <Comment />
+      {!isBasicPath && <Comment />}
     </>
   );
 }
