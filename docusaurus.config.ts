6405b857fdb391944b249ac53d91f9d520a7a530
brown <brown2243@naver.com>
2025-03-12 00:40:33 +0900

    chore: contextualSearch false

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,10 +26,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    // defaultLocale: "ko",
-    // locales: ["ko"],
     defaultLocale: "ko",
-    locales: ["ko", "en"],
+    locales: ["ko"],
   },
 
   presets: [
@@ -67,24 +65,11 @@ const config: Config = {
       },
     ],
     algolia: {
-      // The application ID provided by Algolia
       appId: "E4SFC6MK1N",
       apiKey: "1d930172b420be076364ff5d12e6d306",
       indexName: "braurus",
-      // Optional: see doc section below
-      contextualSearch: true,
-      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      externalUrlRegex: "external\\.com|domain\\.com",
-      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      // replaceSearchResultPathname: {
-      //   from: "/docs/", // or as RegExp: /\/docs\//
-      //   to: "/",
-      // },
-      // Optional: Algolia search parameters
+      contextualSearch: false,
       searchParameters: {},
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: "search",
-      //... other Algolia params
     },
     // Replace with your project's social card
     image: "img/social-card.png",
